fb971c4be2cbc6b9b43a572819f7e9379c29098f
Zain Mehdi <szainmehdi@gmail.com>
2020-01-28 10:08:52 -0500

    Add trait to serialize attributes for entities and value objects.

---
+++ src/Serializer/SerializesAttributes.php
@@ -0,0 +1,57 @@
+<?php declare(strict_types=1);
+
+namespace Zain\LaravelDoctrine\Jetpack\Serializer;
+
+use LaravelDoctrine\ORM\Serializers\ArrayEncoder;
+use Symfony\Component\Serializer\Encoder\JsonEncode;
+use Symfony\Component\Serializer\Encoder\JsonEncoder;
+use Symfony\Component\Serializer\Normalizer\DateTimeNormalizer;
+use Symfony\Component\Serializer\Normalizer\ObjectNormalizer;
+use Symfony\Component\Serializer\Serializer;
+
+trait SerializesAttributes
+{
+    /**
+     * Get the instance as an array.
+     */
+    public function toArray(): array
+    {
+        $normalizers = [$this->getObjectNormalizer(), $this->getDateTimeNormalizer()];
+        $encoders = [new ArrayEncoder()];
+
+        /** @var array $serialized */
+        $serialized = (new Serializer($normalizers, $encoders))->serialize($this, ArrayEncoder::FORMAT);
+        return $serialized;
+    }
+
+    /**
+     * Get the instance in a JSON-able array.
+     */
+    public function jsonSerialize(): array
+    {
+        return $this->toArray();
+    }
+
+    /**
+     * Get the instance as a JSON string
+     *
+     * @return string
+     */
+    public function toJson($options = 0)
+    {
+        $normalizers = [$this->getObjectNormalizer(), $this->getDateTimeNormalizer()];
+        $encoders = [new JsonEncoder(new JsonEncode([JsonEncode::OPTIONS => $options]))];
+        return (new Serializer($normalizers, $encoders))->serialize($this, JsonEncoder::FORMAT);
+    }
+
+    protected function getObjectNormalizer(): ObjectNormalizer
+    {
+        return new ObjectNormalizer();
+    }
+
+    protected function getDateTimeNormalizer(): DateTimeNormalizer
+    {
+        return new DateTimeNormalizer();
+    }
+}
+
